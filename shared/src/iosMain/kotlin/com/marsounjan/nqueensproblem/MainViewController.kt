99ebc044d62ab630ac217635b7b10a62a8f0c4ba
Jan Maršoun <jan.marsoun@gendigital.com>
2026-08-24 23:09:41 +0200

    Fix ios build

--- shared/src/iosMain/kotlin/com/marsounjan/nqueensproblem/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/marsounjan/nqueensproblem/MainViewController.kt
@@ -2,6 +2,7 @@ package com.marsounjan.nqueensproblem
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.marsounjan.nqueensproblem.ui.NQueensApp
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSApplicationSupportDirectory
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSSearchPathForDirectoriesInDomains
@@ -15,6 +16,7 @@ fun initializeAppServices() {
     AppServices.init(applicationSupportDirectory())
 }
 
+@OptIn(ExperimentalForeignApi::class)
 private fun applicationSupportDirectory(): String {
     val path = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, true)
         .first() as String
